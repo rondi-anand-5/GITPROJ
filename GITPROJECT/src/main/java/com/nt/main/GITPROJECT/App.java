992e28f7b3e076593d58fbb56f1ddab476509fc4
rondianand <anandrondi99@gmail.com>
2024-11-08 13:21:26 +0530

    Devolper 1 add a file

--- GITPROJECT/src/main/java/com/nt/main/GITPROJECT/App.java
+++ GITPROJECT/src/main/java/com/nt/main/GITPROJECT/App.java
@@ -6,6 +6,11 @@ package com.nt.main.GITPROJECT;
  */
 public class App 
 {
+	
+	public int sum(int a,int b)
+	{
+	   return a+b;
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- GITPROJECT/src/test/java/com/nt/main/GITPROJECT/AppTest.java
+++ GITPROJECT/src/test/java/com/nt/main/GITPROJECT/AppTest.java
@@ -1,38 +1,45 @@
 package com.nt.main.GITPROJECT;
 
-import junit.framework.Test;
+import org.junit.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
+    private App a;
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
+        a = new App(); // Instantiate App here
+    }
+
+    @Test
+    public void TestSumWithPositive() {
+        int val = 3;
+        int val1 = 5;
+        int expected = val + val1;  // Expected sum
+        int actual = a.sum(val, val1); // Using same values for consistency
+        assertEquals(expected, actual);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static TestSuite suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigorous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
